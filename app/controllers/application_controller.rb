a5357ef54cd18eab1642d75b5a08c7fdd8c6ddda
Lucas Santos Oliveira <obxlucas@gmail.com>
2023-11-13 00:45:56 -0300

    obriga cadastro de pousada após sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :redirect_owners_to_new_guesthouse
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def redirect_owners_to_new_guesthouse
+    return unless user_signed_in? && current_user.owner? && !current_user.guesthouse.present?
+
+    unless request.path == new_guesthouse_path || request.path == destroy_user_session_path
+      flash[:alert] = 'Por favor, cadastre sua pousada'
+      redirect_to new_guesthouse_path
+    end
   end
 end
---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,17 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create, :update]
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+  end
+
+  def after_sign_up_path_for(resources)
+    if resource.owner?
+      new_guesthouse_path
+    else
+      super
+    end
+  end
+end
